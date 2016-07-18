e677244c5a146ebc6d66022faeaac1c8638df99e
xpcomrade <xpcomrade@gmail.com>
2016-07-18 15:38:50 +0800

    create module data-structure

---
+++ data-structure/src/main/java/com/xpcomrade/study/javase/datastructure/Queue.java
@@ -0,0 +1,36 @@
+package com.xpcomrade.study.javase.datastructure;
+
+/**
+ * Created by xpcomrade on 2016/7/12.
+ * Copyright (c) 2016, xpcomrade@gmail.com All Rights Reserved.
+ * Description: TODO(这里用一句话描述这个类的作用). <br/>
+ */
+public class Queue<T> {
+
+    private int default_size = 10;
+
+    private int capacity;
+
+    private  Object[] elementData;
+
+    private int index = 0;
+
+    public Queue(){
+        this.capacity = this.default_size;
+        elementData = new Object[this.capacity];
+    }
+
+    public Queue(int initSize){
+        this.capacity = initSize;
+        elementData = new Object[capacity];
+    }
+
+    public void add(T element) {
+        if (index > capacity -1 ) {
+            throw new IndexOutOfBoundsException("the queue is full!");
+        }
+        elementData[index++] = element;
+    }
+
+
+}
