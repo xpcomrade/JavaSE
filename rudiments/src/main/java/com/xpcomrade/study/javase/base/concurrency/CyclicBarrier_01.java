649542cb616a5ebbc4b04dc4bcaa3c0cda7478ec
xpcomrade <xpcomrade@gmail.com>
2016-09-07 13:57:11 +0800

    new file

---
+++ rudiments/src/main/java/com/xpcomrade/study/javase/base/concurrency/CyclicBarrier_01.java
@@ -0,0 +1,54 @@
+package com.xpcomrade.study.javase.base.concurrency;
+
+import java.util.concurrent.BrokenBarrierException;
+import java.util.concurrent.CyclicBarrier;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+/** 
+ * Created by wangzp
+ * Date: 5:51:56 PM, Nov 14, 2013
+ * Description: TODO(当所有玩家都通过关卡1，才能进入关卡2)
+ */
+public class CyclicBarrier_01 {
+	public static void main(String[] args) {
+		CyclicBarrier cb = new CyclicBarrier(4, new GameBarrier());
+		ExecutorService executor = Executors.newFixedThreadPool(4);
+		for (int i = 0; i < 4; i++) {
+			executor.submit(new Player(cb,i));
+		}
+		executor.shutdown();
+	}
+}
+
+class Player implements Runnable{
+	
+	private CyclicBarrier cyclicBarrier;
+	
+	private int id;
+	
+	public Player(CyclicBarrier cyclicBarrier, int id){
+		this.cyclicBarrier = cyclicBarrier;
+		this.id = id;
+	}
+	
+	@Override
+	public void run() {
+		try {
+			System.out.println("玩家" + id + "正在玩第一关.....");
+			cyclicBarrier.await();
+			System.out.println("玩家" + id + "进入第二关.....");
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		} catch (BrokenBarrierException e) {
+			e.printStackTrace();
+		}
+	}
+}
+
+class GameBarrier implements Runnable {
+	@Override
+	public void run() {
+			System.err.println("所有玩家进入第二关.....");
+	}
+}
---
+++ rudiments/src/main/java/com/xpcomrade/study/javase/base/concurrency/MyTest.java
@@ -0,0 +1,47 @@
+package com.xpcomrade.study.javase.base.concurrency;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Map;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: Jesse
+ * Date: 13-11-26
+ * Time: 下午1:58
+ * To change this template use File | Settings | File Templates.
+ */
+public class MyTest {
+
+    public static void main(String[] args){
+        Map<String, Integer> cakeMap = new HashMap<String,Integer>();
+
+        cakeMap.put("1.2", 2);
+        cakeMap.put("2.2",1);
+        cakeMap.put("2.2",1);
+
+        Map<String,Integer> cardMap = new HashMap<String, Integer>();
+
+        cardMap.put("1.2",1);
+        cardMap.put("2.2", 2);
+
+        Iterator iter = cakeMap.entrySet().iterator();
+
+        while(iter.hasNext()) {
+            Map.Entry<String, Integer> entry = (Map.Entry<String, Integer>)iter.next();
+            if (cardMap.containsKey(entry.getKey())) {
+                if (entry.getValue().intValue() != cardMap.get(entry.getKey())){
+
+
+                    System.out.println("你购买"+entry.getKey()+"磅蛋糕"+entry.getValue()+"个，但你录入了" +
+                            cardMap.get(entry.getKey()) +"张" + entry.getKey()+"磅的积分卡");
+                }
+
+            }
+        }
+
+
+    }
+    
+}
