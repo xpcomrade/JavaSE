05b7aec46261a26c784b74cc43c09c0460ecbed6
xpcomrade <xpcomrade@gmail.com>
2017-03-09 17:58:08 +0800

    add proxy files

---
+++ rudiments/src/main/java/my/javase/study/proxy/AbstractSubject.java
@@ -0,0 +1,11 @@
+package my.javase.study.proxy;
+
+/**
+ * Created by xpcomrade on 2017/3/9.
+ * Copyright (c) 2017, xpcomrade@gmail.com All Rights Reserved.
+ * Description: TODO(这里用一句话描述这个类的作用). <br/>
+ */
+public interface AbstractSubject {
+
+    abstract void request();
+}
